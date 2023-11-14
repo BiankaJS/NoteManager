4de5b27bc4e112dae1d5161ba925baca6db92264
Bianka <bianka.juarez@novacore.mx>
2023-11-13 20:30:57 -0600

    Template Modifided

--- mynotes/models.py
+++ mynotes/models.py
@@ -6,6 +6,7 @@ class Note(models.Model):
   title = models.CharField(max_length=1000)
   content = models.TextField()
   creation_date = models.DateTimeField("creation_date")
+  id = models.AutoField(primary_key=True)
 
   def __str__(self):
     return self.title
